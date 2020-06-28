bd9cff396d0a83be240e5ef7825e5de9fcdcac19
liuxu <lx196069@alibaba-inc.com>
2020-06-28 21:17:45 +0800

    max depth of binary tree, BFS version

--- src/tree/MaxDepthOfBinaryTree.java
+++ src/tree/MaxDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * problem link: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/47/
  */
@@ -23,7 +26,8 @@ class TreeNode {
 }
 public class MaxDepthOfBinaryTree {
     public static int maxDepth(TreeNode root) {
-        return maxDepthUsingRecursion(root);
+        //return maxDepthUsingRecursion(root);
+        return maxDepthUsingBFS(root);
     }
 
     private static int maxDepthUsingRecursion(TreeNode node) {
@@ -32,6 +36,33 @@ public class MaxDepthOfBinaryTree {
         }
 
         return Math.max(maxDepthUsingRecursion(node.left), maxDepthUsingRecursion(node.right)) + 1;
+    }
+
+    private static int maxDepthUsingBFS(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int maxDepth = 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            maxDepth++;
+
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+        }
+
+        return maxDepth;
     }
 
     public static void main(String[] args) {
